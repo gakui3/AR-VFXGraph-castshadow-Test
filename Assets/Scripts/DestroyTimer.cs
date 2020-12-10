70bc47fdae6b7f3865b7cb8e4de8a94b00aa52fb
gKirii <gaku.kirii@gmail.com>
2020-12-10 11:49:32 +0900

    first commit

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Destroy(gameObject, 15f);   
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class Spawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ParticleobjectPrefab;
+
+    [SerializeField]
+    GameObject objectPrefab;
+
+    public TrackableType type;
+
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (raycastManager.Raycast(Input.GetTouch(0).position, hitResults, TrackableType.All))
+            {
+                Instantiate(ParticleobjectPrefab, hitResults[0].pose.position, Quaternion.identity);
+            }
+        }
+    }
+}
